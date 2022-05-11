4500e39899d401b03d7e5fa123a57240b3ad16e2
Julian Camilo Builes Serrano <buiser147@hotmail.com>
2022-05-10 23:38:09 -0500

    cors

--- flask_configurations.py
+++ flask_configurations.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_cors import CORS
 
 application = Flask(__name__)
+CORS(application, supports_credentials=True)
+cors = CORS( resource={ r"/*": { "origins": "*" } } )
+login_manager=LoginManager()
 login_manager=LoginManager()
 login_manager.init_app(application)
 application.config["SECRET_KEY"] = "1dafafghsdsf5378167ugfdsasdfghj98797781234741arfcshzgwffzgnssaerASXMHMRMDwefsrvs8945)(/%#"
