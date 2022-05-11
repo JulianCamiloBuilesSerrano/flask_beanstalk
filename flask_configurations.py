e2a1f74feaebe1d9ccad8fe31cd3d901c7beed92
Julian Camilo Builes Serrano <buiser147@hotmail.com>
2022-05-11 00:58:31 -0500

    cambios

--- flask_configurations.py
+++ flask_configurations.py
@@ -11,3 +11,4 @@ login_manager.init_app(application)
 application.config["SECRET_KEY"] = "1dafafghsdsf5378167ugfdsasdfghj98797781234741arfcshzgwffzgnssaerASXMHMRMDwefsrvs8945)(/%#"
 application.secret_key = "test_secret"
 application.config["SESSION_TYPE"] = "filesystem"
+application.config['TESTING'] = False
