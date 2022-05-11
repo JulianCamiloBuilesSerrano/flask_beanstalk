325fc5ad56fa38eb08b4b4120ab1e10e67b94af7
Julian Camilo Builes Serrano <buiser147@hotmail.com>
2022-05-10 21:27:14 -0500

    primer commit

--- application.py
+++ application.py
@@ -1,6 +1,11 @@
 
-from flask import Flask, jsonify, make_response
+from flask import Flask, current_app, jsonify, make_response, request
+from flask_login import LoginManager
+from werkzeug.security import check_password_hash
+from flask_login import LoginManager
+from flask_login import login_user, login_required, logout_user
 
+from models import *
 # # print a nice greeting.
 # def say_hello(username = "World"):
 #     return '<p>Hello %s!</p>\n' % username
@@ -17,7 +22,10 @@ from flask import Flask, jsonify, make_response
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
-
+login_manager=LoginManager()
+login_manager.init_app(application)
+application.config["SECRET_KEY"] = "1dafafghsdsf5378167ugfdsasdfghj98797781234741arfcshzgwffzgnssaerASXMHMRMDwefsrvs8945)(/%#"
+application.secret_key = "test_secret"
 # # add a rule for the index page.
 # application.add_url_rule('/', 'index', (lambda: header_text +
 #     say_hello() + instructions + footer_text))
@@ -28,9 +36,33 @@ application = Flask(__name__)
 #     header_text + say_hello(username) + home_link + footer_text))
 
 # # run the app.
+
+@login_manager.user_loader
+def load_user(user_name):
+    return Usuario.get_user(user_name)
 @application.route("/")
 def main():
     return make_response(jsonify({"respuesta":"hola"}),200)
+
+@application.route("/login", methods=["POST"])
+def login():
+
+    req_username = request.json["user"]
+    req_password = request.json["password"]
+    user = Usuario.get_user(req_username)
+    
+    if user == None:
+        return make_response(jsonify("Usuario no existe"), 400)
+
+    if not check_password_hash(user.password, req_password):
+        return make_response(jsonify("contraseña incorrecta"), 403)
+    current_app.logger.info(f"Usuario {req_username} logueado")
+    login_user(user)
+
+    response_user = {'username': user.user, 'id': user.id,
+                     "rol":user.type}
+
+    return make_response(jsonify(response_user), 200)
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
---
+++ models.py
@@ -0,0 +1,350 @@
+"""En el siguiente scrip se va a encontrar los modelos de clases para ser mapeadas 
+en MySql con sqlalchemy 
+"""
+from shutil import ExecError
+
+from datetime import date, datetime, timedelta
+from flask import current_app
+import db
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+    DateTime,
+    Table,
+    Float,
+    Time,
+    Date,
+    Boolean,
+    insert,
+    select,
+)
+
+from sqlalchemy.orm import relationship
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash
+
+porfesor_x_clase = Table(
+    "profesorXclase",
+    db.Base.metadata,
+    Column("profesor_id", ForeignKey("usuario.id"), primary_key=True),
+    Column("clase_id", ForeignKey("clase.id"), primary_key=True),
+)
+estudiante_x_clase = Table(
+    "estudianteXclase",
+    db.Base.metadata,
+    Column("estudiante_id", ForeignKey("usuario.id"), primary_key=True),
+    Column("clase_id", ForeignKey("clase.id"), primary_key=True),
+)
+
+
+class Usuario(UserMixin, db.Base):
+    """
+    Clase usuario, dentro de esta clase se
+    encuentra el valor type el cual ayuda a
+    identificar si el usuario es profesor o estudiante
+    Args:
+        db (declarative_base): permite realizar el mapeo a la base de datos
+    """
+
+    __tablename__ = "usuario"
+    id = Column(Integer, primary_key=True)
+    user = Column(String(20), nullable=False, unique=True)
+    password = Column(String(200), nullable=False)
+    name = Column(String(20), nullable=False)
+    last_name = Column(String(20), nullable=False)
+    type = Column(String(10), nullable=False)
+    
+    authenticated = Column(Boolean, default=False)
+   
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.user
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
+
+    def create_user(user, password, name, last_name, type_user):
+        with db.engie.connect() as connection:
+            connection.execute(
+                insert(Usuario).values(
+                    user=user,
+                    password=generate_password_hash(password),
+                    name=name,
+                    last_name=last_name,
+                    type=type_user,
+                )
+            )
+            connection.close()
+            
+
+    def get_user(user):
+        with db.Session.begin() as session:
+            response = session.query(Usuario).where(Usuario.user == user).first()
+            session.close()
+        return response
+    # Eod
+
+    def get_teacher_courses(user_id):
+
+        query = """select * 
+                   from curso
+                   where id in (
+                       select curso_id 
+                       from clase 
+                       where id in (
+                           select clase_id 
+                           from profesorXclase, usuario 
+                           where profesorXclase.profesor_id = {} and usuario.id = {}
+                       )
+                   );""".format(user_id, user_id)
+
+        with db.engie.connect() as connection: 
+            result = connection.execute(query)
+        # Eow
+
+        return result.all()
+    # Eod
+
+    def get_actual_sesion_estudiante(self):
+        with db.engie.connect() as connection:
+            result = connection.execute(
+                """select bd_tesis.sesion.id,bd_tesis.sesion.hora_inicio,bd_tesis.sesion.hora_fin,dia 
+                from bd_tesis.estudianteXclase,bd_tesis.clase,bd_tesis.horario,bd_tesis.sesion 
+                where
+                bd_tesis.estudianteXclase.estudiante_id = {} and 
+                bd_tesis.clase.id = bd_tesis.estudianteXclase.clase_id and 
+                bd_tesis.horario.clase_id =  bd_tesis.clase.id and
+                bd_tesis.sesion.clase_id = bd_tesis.clase.id;""".format(
+                    self.id
+                )
+            )
+        today = datetime.today()
+        for row in result.fetchall():
+            if row["hora_inicio"] <  today < row["hora_fin"]:
+                return row["id"]
+                
+        return None
+    # Eod
+
+    def get_course_sessions(course_id): 
+        query = """SELECT clase.id AS clase_id, sesion.hora_inicio, sesion.hora_fin, sesion.id AS sesion_id
+                   FROM clase, curso, sesion
+                   WHERE clase.curso_id = 3194 AND curso.id = clase.curso_id AND sesion.clase_id = clase.id;""".format(course_id)
+
+        with db.engie.connect() as connection: 
+            result = connection.execute(query)
+        # Eow
+        return result.all() 
+    # Eod
+
+    def get_actual_sesion_profesor(self):
+        with db.engie.connect() as connection:
+            print("id profesor: ", self.id)
+            result = connection.execute(
+                """select bd_tesis.sesion.id,bd_tesis.sesion.hora_inicio,bd_tesis.sesion.hora_fin,dia 
+                from bd_tesis.profesorXclase,bd_tesis.clase,bd_tesis.horario,bd_tesis.sesion 
+                where bd_tesis.profesorXclase.profesor_id = {} and 
+                bd_tesis.clase.id = bd_tesis.profesorXclase.clase_id and 
+                bd_tesis.horario.clase_id =  bd_tesis.clase.id and
+                bd_tesis.sesion.clase_id = bd_tesis.clase.id;
+                """.format(
+                    self.id
+                )
+            )
+        today = datetime.today()
+        for row in result.fetchall():
+            if row["hora_inicio"] <  today < row["hora_fin"]:
+                return row["id"]
+                
+        return None
+
+class Curso(db.Base):
+    """
+    Clase curso, contiene la informacion
+    necesaria para identificar un curso
+    Args:
+        db (declarative_base): permite realizar el mapeo a la base de datos
+    """
+
+    __tablename__ = "curso"
+
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(30), nullable=False)
+    fecha_inicio_semestre = Column(Date, nullable=False)
+    fecha_fin_semestre = Column(Date, nullable=False)
+
+
+class Clase(db.Base):
+    """
+    Contiene la informacion
+    necesaria para identificar un clase
+    Args:
+        db (declarative_base): permite realizar el mapeo a la base de datos
+    """
+
+    __tablename__ = "clase"
+
+    id = Column(Integer, primary_key=True)
+    cantidad_estudiantes = Column(Integer)
+    curso_id = Column(Integer, ForeignKey("curso.id"))
+
+
+class Emocion(db.Base):
+    """
+    Clase emocion, contiene la informacion
+    necesaria para identificar las diferentes emociones,
+    esto permitira almacenar de mejor manera las emociones y tener mas
+    control de cuales son
+    Args:
+        db (declarative_base): permite realizar el mapeo a la base de datos
+    """
+
+    __tablename__ = "emocion"
+
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(10), nullable=False)
+
+
+class Sesion(db.Base):
+    """
+    Clase sesion, contiene la informacion
+    necesaria para identificar una sesion
+    Args:
+        db (declarative_base): permite realizar el mapeo a la base de datos
+    """
+
+    __tablename__ = "sesion"
+
+    id = Column(Integer, primary_key=True)
+    hora_inicio = Column(DateTime, nullable=False)
+    hora_fin = Column(DateTime, nullable=False)
+    clase_id = Column(Integer, ForeignKey("clase.id"))
+    
+    def get_sesion(id):
+        with db.Session.begin() as session:
+            response = session.query(Sesion).where(Sesion.id == id).first()
+            session.close()
+        return response
+    
+
+
+class Emocion_x_Estudiante(db.Base):
+    """
+    Clase Emocion_x_Estudiante, contiene la informacion
+    necesaria para identificar el tiempo, la emoción y la sesion a la cual el
+    estudiante dío la información de la emoción
+    Args:
+        db (declarative_base): permite realizar el mapeo a la base de datos
+    """
+
+    __tablename__ = "emocionXestudiante"
+
+    estudiante_id = Column(Integer, ForeignKey("usuario.id"), primary_key=True)
+    sesion_id = Column(Integer, ForeignKey("sesion.id"), primary_key=True)
+    fecha = Column(DateTime, primary_key=True)
+    emocion_id = Column(Integer, ForeignKey("emocion.id"), primary_key=True)
+    porcentaje = Column(Float)
+
+    def get_emocion_x_estudiante(sesion_id):
+        with db.engie.connect() as connection:
+            now = datetime.today()
+            # time_d = 10
+            # if now.second < time_d:             
+            #     seconds = now.second + (60 - time_d)
+            #     if now.minute == 0:
+            #         minutes = now.minute + (60 - 1)
+            #         hours = now.hour - 1
+            #         previous = now.replace(second = seconds, minute= minutes, hour = hours)
+            #     minutes = now.minute - 1
+            #     previous = now.replace(second = seconds, minute= minutes)
+            # else:
+            #     seconds = now.second - time_d
+            #     previous =now.replace(second = seconds)
+            
+            if(now.second > 9):
+                second_before = int(str(now.second)[0]+"0")
+                second_after = int(str(now.second)[0]+"0") + 10
+            else:
+                second_before = 0
+                second_after = 10
+            before = datetime(year=now.year,month=now.month,day=now.day,hour=now.hour,minute=now.minute, second=second_before)
+            if second_after != 60:
+                previous = datetime(year=now.year,month=now.month,day=now.day,hour=now.hour,minute=now.minute, second=second_after)
+            else:
+                previous = datetime(year=now.year,month=now.month,day=now.day,hour=now.hour,minute=now.minute+1, second=0)
+            respuesta = connection.execute("""
+                                           SELECT * FROM bd_tesis.emocionXestudiante where bd_tesis.emocionXestudiante.sesion_id = {} and (bd_tesis.emocionXestudiante.fecha between '{}' and '{}');
+                            """.format(sesion_id, before - timedelta(seconds=10), previous -timedelta(seconds=10)))
+            connection.close()
+        return respuesta.fetchall()
+
+    def insert_emocion_estudiante(estudiante_id,sesion_id,fecha,emocion,porcentaje):
+        if(fecha.second > 9):
+            second = int(str(fecha.second)[0]+"0")
+        else:
+            second = 0
+        new_fecha =datetime(year=fecha.year,month=fecha.month,day=fecha.day,hour=fecha.hour,minute=fecha.minute, second=second)
+        try:
+            with db.engie.connect() as connection:
+                respuesta = connection.execute(""" 
+                                select id 
+                                from bd_tesis.emocion where emocion.nombre = '{}';
+                                """.format(emocion))
+                emocion_id = respuesta.fetchone()["id"]
+                connection.execute(
+                    insert(Emocion_x_Estudiante).values(
+                        estudiante_id=estudiante_id,
+                        sesion_id=sesion_id,
+                        fecha=new_fecha,
+                        emocion_id=emocion_id,
+                        porcentaje=porcentaje,
+                    )
+                )
+                connection.close()
+        except Exception as err:
+            current_app.logger.error("Error en guardar la emocion del estudiante")
+    def get_emocions_for_sesion(sesion_id):
+        try:
+            with db.engie.connect() as connection:
+                respuesta =  connection.execute("""
+                                                select  bd_tesis.usuario.name,  bd_tesis.usuario.last_name,bd_tesis.emocion.nombre,bd_tesis.emocionXestudiante.fecha  
+                                                from bd_tesis.emocionXestudiante,bd_tesis.emocion,bd_tesis.usuario 
+                                                where 
+                                                bd_tesis.emocionXestudiante.sesion_id = {} and 
+                                                bd_tesis.emocionXestudiante.estudiante_id = bd_tesis.usuario.id and 
+                                                bd_tesis.emocionXestudiante.emocion_id = bd_tesis.emocion.id ;""".format(sesion_id))
+            return respuesta.fetchall()
+        except Exception as err:
+            current_app.logger.error("Error al traer los datos")
+            
+
+class Horario(db.Base):
+    """
+    Clase horari, contiene la informacion
+    necesaria para identificar los horarios que puede tener una clase
+    Args:
+        db (declarative_base): permite realizar el mapeo a la base de datos
+    """
+
+    __tablename__ = "horario"
+
+    clase_id = Column(Integer, ForeignKey("clase.id"), primary_key=True)
+    dia = Column(String(15), primary_key=True)
+    hora_inicio = Column(Time)
+    hora_fin = Column(Time)
+
+
+#db.Base.metadata.create_all(db.engie)
+# Usuario.create_user(user="simondavila",password="Banfield2019",name="Simon",last_name="Davila",type_user="estudiante")
+# #print(Usuario.get_user("uzsdg4"))
